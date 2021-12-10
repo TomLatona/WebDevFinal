42a8fa3238755c3a3f78979b833bd7cecf9822ce
Tom Latona <latona.tom1@gmail.com>
2021-12-10 14:10:14 -0500

    CORS error attempt 2

--- index.js
+++ index.js
@@ -12,14 +12,23 @@ app.use('/api', api_routes);
 //serve static front-end
 app.use(express.static('front_end'));
 
-//to allow access from other machines, CORS error
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+// //to allow access from other machines, CORS error
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", '*');
+//     res.header("Access-Control-Allow-Credentials", true);
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+//     next();
+// });
+
+const cors = require("cors");
+const corsOptions = {
+	origin: '*',
+	credentials: true,
+	optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 
 //start server
 app.listen(port, () => console.log(`App running on port ${port}`));
