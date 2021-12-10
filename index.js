850803ee665328640e83c0740c347821145a3354
Tom Latona <latona.tom1@gmail.com>
2021-12-09 19:12:15 -0500

    Changed how static front end is served

--- index.js
+++ index.js
@@ -3,18 +3,14 @@
 //initialize server
 const express = require('express');
 const app = express();
-const port = 5000;
-//app.use(express.static('public'))
+const port = 3000;
+//const port = process.env.PORT || 80;
 
 var api_routes = require('./api_routes.js');
 app.use('/api', api_routes);
 
 //serve static front-end
-app.use('/demo', express.static('front_end'));
-
-app.get("", (req, res) => {
-	res.send("hello world");
-});
+app.use(express.static('front_end'));
 
 //start server
 app.listen(port, () => console.log(`App running on port ${port}`));
